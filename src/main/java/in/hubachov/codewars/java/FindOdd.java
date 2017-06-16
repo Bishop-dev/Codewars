ce28cdbad1f7b80081396e42eec156418b818f9a
Bishop-dev <alex.bishop@yandex.ru>
2017-06-16 08:50:11 +0300

    013 - Find odd occurences

---
+++ src/main/java/in/hubachov/codewars/java/FindOdd.java
@@ -0,0 +1,38 @@
+package in.hubachov.codewars.java;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Given an array, find the int that appears an odd number of times.
+
+ There will always be only one integer that appears an odd number of times.
+
+
+ * Created by Sashko on 6/16/17.
+ */
+public class FindOdd {
+
+/*
+	public static int findIt(int[] A) {
+	    int xor = 0;
+	    for (int i = 0; i < A.length; i++) {
+	        xor ^= A[i];
+	    }
+	    return xor;
+    }
+
+	public static int findIt(int[] arr) {
+		return Arrays.stream(arr).reduce(0, (x, y) -> x ^ y);
+	}
+	*/
+	public static int findIt(int[] A) {
+		final Map<Integer, Long> map = Arrays
+				.stream(A)
+				.mapToObj(Integer::new)
+				.collect(Collectors.groupingBy(n -> n, Collectors.counting()));
+		return map.entrySet().stream().filter(n -> n.getValue() % 2 != 0).findFirst().get().getKey();
+	}
+
+}
